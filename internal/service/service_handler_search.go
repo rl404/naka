6cbeb7861e32fbe11b52eb1dd50c44011dd863bd
axel <axel.rl.404@gmail.com>
2022-08-21 17:11:16 +0700

    feat: queue multiple youtube url

--- internal/service/service_handler_search.go
+++ internal/service/service_handler_search.go
@@ -20,58 +20,64 @@ func (s *service) searchSong(ctx context.Context, m *discordgo.MessageCreate, g 
 
 	// If using youtube url.
 	if s.youtube.IsURLValid(args[0]) {
-		videoID, err := s.youtube.GetIDFromURL(ctx, args[0])
-		if err != nil {
-			if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+		for _, arg := range args {
+			if !s.youtube.IsURLValid(arg) {
+				continue
+			}
+
+			videoID, err := s.youtube.GetIDFromURL(ctx, arg)
+			if err != nil {
+				if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+					return errors.Wrap(ctx, err)
+				}
 				return errors.Wrap(ctx, err)
 			}
-			return errors.Wrap(ctx, err)
-		}
 
-		sourceURL, err := s.youtube.GetSourceURLByID(ctx, videoID)
-		if err != nil {
-			if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+			sourceURL, err := s.youtube.GetSourceURLByID(ctx, videoID)
+			if err != nil {
+				if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+					return errors.Wrap(ctx, err)
+				}
 				return errors.Wrap(ctx, err)
 			}
-			return errors.Wrap(ctx, err)
-		}
 
-		video, err := s.youtube.GetVideo(ctx, videoID)
-		if err != nil {
-			if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+			video, err := s.youtube.GetVideo(ctx, videoID)
+			if err != nil {
+				if _, err := s.discord.SendMessage(ctx, m.ChannelID, entity.InvalidYoutubeURL); err != nil {
+					return errors.Wrap(ctx, err)
+				}
 				return errors.Wrap(ctx, err)
 			}
-			return errors.Wrap(ctx, err)
-		}
 
-		if err := s.queue.Add(ctx, g.ID, queueEntity.Song{
-			Title:        video.Title,
-			URL:          s.youtube.GenerateVideoURL(videoID),
-			ChannelTitle: video.ChannelTitle,
-			ChannelURL:   s.youtube.GenerateChannelURL(video.ChannelID),
-			Image:        video.Image,
-			Duration:     video.Duration,
-			View:         video.View,
-			Like:         video.Like,
-			Dislike:      video.Dislike,
-			SourceURL:    sourceURL,
-		}); err != nil {
-			return errors.Wrap(ctx, err)
-		}
+			if err := s.queue.Add(ctx, g.ID, queueEntity.Song{
+				Title:        video.Title,
+				URL:          s.youtube.GenerateVideoURL(videoID),
+				ChannelTitle: video.ChannelTitle,
+				ChannelURL:   s.youtube.GenerateChannelURL(video.ChannelID),
+				Image:        video.Image,
+				Duration:     video.Duration,
+				View:         video.View,
+				Like:         video.Like,
+				Dislike:      video.Dislike,
+				SourceURL:    sourceURL,
+			}); err != nil {
+				return errors.Wrap(ctx, err)
+			}
 
-		if _, err := s.discord.SendMessageEmbed(ctx, m.ChannelID, s.template.GetAddQueue(entity.Video{
-			Title:        video.Title,
-			URL:          s.youtube.GenerateVideoURL(videoID),
-			ChannelTitle: video.ChannelTitle,
-			ChannelURL:   s.youtube.GenerateChannelURL(video.ChannelID),
-			Image:        video.Image,
-			Duration:     video.Duration,
-			View:         video.View,
-			Like:         video.Like,
-			Dislike:      video.Dislike,
-			QueueI:       len(s.queue.GetList(ctx, g.ID)),
-		})); err != nil {
-			return errors.Wrap(ctx, err)
+			if _, err := s.discord.SendMessageEmbed(ctx, m.ChannelID, s.template.GetAddQueue(entity.Video{
+				Title:        video.Title,
+				URL:          s.youtube.GenerateVideoURL(videoID),
+				ChannelTitle: video.ChannelTitle,
+				ChannelURL:   s.youtube.GenerateChannelURL(video.ChannelID),
+				Image:        video.Image,
+				Duration:     video.Duration,
+				View:         video.View,
+				Like:         video.Like,
+				Dislike:      video.Dislike,
+				QueueI:       len(s.queue.GetList(ctx, g.ID)),
+			})); err != nil {
+				return errors.Wrap(ctx, err)
+			}
 		}
 
 		if play {
